e6cc508a43142c08d485f5566eb1771751f2f450
Luke Crest <bromstargames@gmail.com>
2018-03-30 22:49:36 +0200

    Basic functions

--- app/src/main/java/com/example/luke/wifidirect/MainActivity.java
+++ app/src/main/java/com/example/luke/wifidirect/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.luke.wifidirect;
 
+import android.content.Context;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    WifiP2pManager mWifiP2pManager;
+    WifiP2pManager.Channel mChannel;
+    Receiver mReceiver;
+    IntentFilter filter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mWifiP2pManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mWifiP2pManager.initialize(this, getMainLooper(), null);
+        mReceiver = new Receiver(mWifiP2pManager, mChannel);
+
+        filter = new IntentFilter();
+        filter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        filter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        filter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        filter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
     }
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        registerReceiver(mReceiver, filter);
+    }
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        unregisterReceiver(mReceiver);
+    }
+
+
 }
--- app/src/main/java/com/example/luke/wifidirect/Receiver.java
+++ app/src/main/java/com/example/luke/wifidirect/Receiver.java
@@ -8,16 +8,26 @@ import android.net.wifi.p2p.WifiP2pManager;
 public class Receiver extends BroadcastReceiver {
     private WifiP2pManager mWifiP2pManager;
     private WifiP2pManager.Channel mChannel;
+    boolean isReady;
     Receiver(WifiP2pManager m, WifiP2pManager.Channel c)
     {
         this.mWifiP2pManager=m;
         this.mChannel=c;
+        isReady = false;
     }
     @Override
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
         if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
-
+            int state  = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+            if(state==WifiP2pManager.WIFI_P2P_STATE_ENABLED)
+            {
+                isReady=true;
+            }
+            else if(state==WifiP2pManager.WIFI_P2P_STATE_DISABLED)
+            {
+                //disabled
+            }
         } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
 
         } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
